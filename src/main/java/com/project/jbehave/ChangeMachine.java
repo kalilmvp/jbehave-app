ce71fe8304737c6b0bf77a888ecc6d149f2a0fd6
Kalil Peixoto <kalilmvp@gmail.com>
2015-05-25 22:36:10 -0300

    Criado first step para testes de troco de moedas

--- src/main/java/com/project/jbehave/ChangeMachine.java
+++ src/main/java/com/project/jbehave/ChangeMachine.java
@@ -1,5 +1,10 @@
 package com.project.jbehave;
 
+import java.util.List;
+
 public class ChangeMachine {
 
+	public List<Integer> getCoinsForChangeOf(double value) {
+		return null;
+	}
 }
--- src/test/java/com/project/jbehave/FindChangesSteps.java
+++ src/test/java/com/project/jbehave/FindChangesSteps.java
@@ -1,5 +1,34 @@
 package com.project.jbehave;
 
-public class FindChangesSteps {
+import java.util.List;
 
+import static junit.framework.Assert.*;
+
+import org.jbehave.core.annotations.Alias;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+public class FindChangesSteps {
+	
+	private ChangeMachine changeMachine = null;
+	private List<Integer> change = null;
+	
+	@Given("a change machine")
+	public void creteChangeMachine() {
+		this.changeMachine = new ChangeMachine();
+	}
+	
+	@When("i ask for a change of <value>")
+	@Alias("I ask for a change of $value")
+	public void changeFor(@Named("value") double value) {
+		this.change = this.changeMachine.getCoinsForChangeOf(value);
+	}
+	
+	@Then("it returns the <coin> coin")
+	@Alias("it returns the $coin coin")
+	public void changeCoins(@Named("coin") String coins) {
+		assertEquals(this.change.get(0) + "c", coins);
+	}
 }
